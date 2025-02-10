8367cef81b96b37100c1539b55d5e2656de9742e
acgodson <anigodson20@gmail.com>
2025-02-10 02:23:29 +0100

    cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
 
-export default {
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,9 +10,22 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        aurora: "aurora 80s linear infinite",
+      },
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition:
+              "50% 50%, 0% 0%, 25% 25%, 50% 50%, 75% 75%, 100% 100%",
+          },
+          to: {
+            backgroundPosition:
+              "50% 50%, 100% 100%, 125% 125%, 150% 150%, 175% 175%, 200% 200%",
+          },
+        },
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
         primary: "var(--medusa-gold)",
         "primary-foreground": "var(--medusa-black)",
         secondary: "var(--medusa-black)",
@@ -20,5 +35,18 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 } satisfies Config;
+
+export default config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
