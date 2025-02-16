2eff9606ec0b61e5b3cbaed86a85355ce2d3b226
acgodson <anigodson20@gmail.com>
2025-02-16 21:25:49 +0100

    fix not-found module

--- src/lib/medusa/zee/tools/index.ts
+++ src/lib/medusa/zee/tools/index.ts
@@ -1,5 +1,5 @@
 import { createStorageTool } from "./src/GreenfieldTool";
 import { createPrivyWalletTool } from "./src/privyWalletTool";
-import { createProcessingTool } from "./src/GreenfieldStorage";
+import { createProcessingTool } from "./src/sensorProcessingTool";
 
 export { createStorageTool, createPrivyWalletTool, createProcessingTool };
